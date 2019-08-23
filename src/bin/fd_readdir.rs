e3bbd9ec953a92a0db9e64fb9c71b266f65bf840
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2019-08-23 09:27:44 +0200

    Add a check for ino.

--- src/bin/fd_readdir.rs
+++ src/bin/fd_readdir.rs
@@ -1,6 +1,6 @@
 use libc;
 use misc_tests::open_scratch_directory;
-use misc_tests::wasi_wrappers::wasi_fd_readdir;
+use misc_tests::wasi_wrappers::{wasi_fd_filestat_get, wasi_fd_readdir};
 use std::{cmp::min, env, mem, process, slice, str};
 
 const BUF_LEN: usize = 256;
@@ -38,7 +38,7 @@ impl<'a> Iterator for ReadDir<'a> {
             // NOTE Linux syscall returns a NULL-terminated name, but WASI doesn't
             let namelen = dirent.d_namlen as usize;
             let slice = slice::from_raw_parts(name_ptr, namelen);
-            let name = str::from_utf8(slice).unwrap().to_owned();
+            let name = str::from_utf8(slice).expect("invalid utf8").to_owned();
 
             // Update the internal state
             let delta = mem::size_of_val(&dirent) + namelen;
@@ -50,6 +50,14 @@ impl<'a> Iterator for ReadDir<'a> {
 }
 
 fn test_fd_readdir(dir_fd: libc::__wasi_fd_t) {
+    let mut stat: libc::__wasi_filestat_t = unsafe { mem::zeroed() };
+    let status = wasi_fd_filestat_get(dir_fd, &mut stat);
+    assert_eq!(
+        status,
+        libc::__WASI_ESUCCESS,
+        "reading scratch directory stats"
+    );
+
     let mut buf: [u8; BUF_LEN] = [0; BUF_LEN];
     let mut bufused = unsafe { mem::zeroed() };
     let status = wasi_fd_readdir(dir_fd, &mut buf, BUF_LEN, 0, &mut bufused);
@@ -67,6 +75,8 @@ fn test_fd_readdir(dir_fd: libc::__wasi_fd_t) {
         libc::__WASI_FILETYPE_REGULAR_FILE,
         "first type"
     ); // WHY??
+    assert_eq!(dir.dirent.d_ino, stat.st_ino);
+    assert_eq!(dir.dirent.d_namlen, 1);
 
     // the second entry should be `..`
     let dir = dirs.next().expect("second entry is None");
