7b623435fd1fab3d94d108b873520617199bbd31
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2019-10-04 11:00:18 +0200

    Check if CLOCK_MONOTONIC is actually monotonic

--- src/bin/clock_time_get.rs
+++ src/bin/clock_time_get.rs
@@ -1,3 +1,4 @@
+use more_asserts::assert_le;
 use wasi::wasi_unstable;
 use wasi_misc_tests::wasi_wrappers::wasi_clock_time_get;
 
@@ -6,19 +7,28 @@ unsafe fn test_clock_time_get() {
     // desirable to expose high-precision timers, it should still succeed.
     // clock_res_get is where information about precision can be provided.
     let mut time: wasi_unstable::Timestamp = 0;
-    let mut status = wasi_clock_time_get(wasi_unstable::CLOCK_MONOTONIC, 0, &mut time);
-    assert_eq!(
-        status,
-        wasi_unstable::raw::__WASI_ESUCCESS,
-        "clock_time_get with a precision of 0"
-    );
-
-    status = wasi_clock_time_get(wasi_unstable::CLOCK_MONOTONIC, 1, &mut time);
+    let status = wasi_clock_time_get(wasi_unstable::CLOCK_MONOTONIC, 1, &mut time);
     assert_eq!(
         status,
         wasi_unstable::raw::__WASI_ESUCCESS,
         "clock_time_get with a precision of 1"
     );
+
+    let status = wasi_clock_time_get(wasi_unstable::CLOCK_MONOTONIC, 0, &mut time);
+    assert_eq!(
+        status,
+        wasi_unstable::raw::__WASI_ESUCCESS,
+        "clock_time_get with a precision of 0"
+    );
+    let first_time = time;
+
+    let status = wasi_clock_time_get(wasi_unstable::CLOCK_MONOTONIC, 0, &mut time);
+    assert_eq!(
+        status,
+        wasi_unstable::raw::__WASI_ESUCCESS,
+        "clock_time_get with a precision of 0"
+    );
+    assert_le!(first_time, time, "CLOCK_MONOTONIC should be monotonic");
 }
 
 fn main() {
